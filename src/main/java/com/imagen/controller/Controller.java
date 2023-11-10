5f1647afe92c7620ff015b3611ae9d61009da4cd
Ramon <ramon.cruz187@gmail.com>
2023-11-10 17:21:04 -0300

    cors

--- src/main/java/com/imagen/controller/Controller.java
+++ src/main/java/com/imagen/controller/Controller.java
@@ -17,6 +17,7 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@CrossOrigin 
 public class Controller {
 
     @Value("${image.storage.path}")
